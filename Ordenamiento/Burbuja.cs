7ed6aa05ea1c6b5c363117b7eeed4919f69e0b47
Sernandezglez <shernander14@ucol.mx>
2019-04-01 21:34:31 -0600

    Adicion de casos de prueba y clase ordenamiento arreglada

--- Ordenamiento/Burbuja.cs
+++ Ordenamiento/Burbuja.cs
@@ -7,7 +7,18 @@ namespace Ordenamiento
     public class Burbuja
     {
         public void Ordenar(int[]arr){
-            throw new NotImplementedException("Crear los casos de prueba");
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int aux = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = aux;
+                    }
+                }
+            }
         }
     }
 }
